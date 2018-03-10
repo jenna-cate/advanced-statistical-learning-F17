bb2a19a1e0ed957016663dfc6a557c6b6553f9c9
Jenna <33360492+jenna-cate@users.noreply.github.com>
2018-03-10 16:34:04 -0600

    Update and rename 2.6 real estate pred.R to Ridge regression prediction.R

--- Ridge regression prediction.R
+++ Ridge regression prediction.R
@@ -1,22 +1,20 @@
+### A ridge regression (weight decay) model to predict the price of real estate. ###
+
 library(MASS)
 library(glmnet)
 
 realestate_test <- read_csv("~/Documents/ASL HW 2/realestate-test.csv")
 realestate_train <- read_csv("~/Documents/ASL HW 2/realestate-train.csv")
 
-y <- realestate_train$price
-x <- model.matrix(price~., realestate_train)
-newx <- model.matrix(~., realestate_test)
+y <- realestate_train$price # pull out prices from the training data - what we're trying to predict
+x <- model.matrix(price~., realestate_train)  # convert categorical variables to dummy variables
+newx <- model.matrix(~., realestate_test)  # and do the same to the test data
+lam.seq = exp(seq(log(100),log(1e-5),length=500))  # sequence of lambdas (penalty on feature weights/regression coeffs) to optimize
 
-lam.seq = exp(seq(log(100),log(1e-5),length=500))
 lm.model <- lm.ridge(y~x-1, lambda=lam.seq)   # ridge regression model
 
 beta = coef(lm.model)                   # matrix of estimated coefficients (for each lambda)
 penalty = rowSums(beta[,-1]^2)   # total penalty P(\beta) (sum of squared betas)
-
-ridge.stuff = data.frame(lam = lam.seq, intercept=beta[,1], x1=beta[,2],
-                         x2=beta[,3], penalty=penalty, GCV=m$GCV, 
-                         row.names=NULL)
 
 library(glmnet)
 glm.model <- glmnet(x, y, alpha=0, lambda=lam.seq)
@@ -36,4 +34,6 @@ yhat <- predict(cv.gm, newx, s="lambda.min")
 yhat.tr <- predict(cv.gm, x, s="lambda.min")
 error <- y-yhat.tr
 rmse.tr <- sqrt(mean(error^2))
-write.csv(yhat, file = "Predictions.csv")
+#write.csv(yhat, file = "Predictions.csv")
+
+## RMSE when predicting against prof's test data: 45.07660
